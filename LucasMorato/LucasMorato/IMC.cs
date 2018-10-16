9a9ecc3050d2ec5b4c14d249732c2e34b3907c6f
Lucas Morato <36897352+morato13@users.noreply.github.com>
2018-10-16 20:31:30 -0300

    Add files via upload

---
+++ LucasMorato/LucasMorato/IMC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LucasMorato
+{
+    class IMC
+    {
+        private double peso;
+        private double altura;
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+        }
+
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+
+        public double Classifcar(double imc)
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ LucasMorato/LucasMorato/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LucasMorato
+{
+    class Program
+    {
+        static int Maior(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+                return b;
+        }
+
+        static int Maior2(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
+        static bool Resto(int c)
+        {
+            if (c % 2 == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            //Questão 1
+            Console.WriteLine(Maior(1, 2));
+            Console.WriteLine(Maior2(5, 2));
+
+            //Questão 3
+            Console.WriteLine(Resto(2));
+        
+
+           
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+
+    }
+}
+
